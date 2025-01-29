e9ae1b0da898993c647f175f015cf5ac7e074f10
Rawlings_10 <rawllingsoduwa@gmail.com>
2025-01-28 20:35:30 -0500

    Update HelloWorld.cpp
    class and constructor created

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,21 +1,32 @@
-// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <limits> // For numeric_limits
 
-int main()
+using namespace std;
+
+class UserAccount
 {
-    std::cout << "Hello World!" <<"\n\n";
-    std::cout << "Rawlings"; //this is a comment test
-}
+public:
+	int ID;
+	string USERNAME;
+	string PASSWORD;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+	//constructor for the class UserAcount
+	UserAccount(int id, string userName, string passWord)
+		: ID(id), USERNAME(userName), PASSWORD(passWord) {}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+};
+
+int main() {
+
+};
+	// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+	// Debug program: F5 or Debug > Start Debugging menu
+
+	// Tips for Getting Started: 
+	//   1. Use the Solution Explorer window to add/manage files
+	//   2. Use the Team Explorer window to connect to source control
+	//   3. Use the Output window to see build output and other messages
+	//   4. Use the Error List window to view errors
+	//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+	//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
